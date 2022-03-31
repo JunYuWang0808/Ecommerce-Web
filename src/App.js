21b0409c57da9f6ab8d9e5860356c825e0e96c84
Wang JunYu <junyu@Wangs-MacBook-Pro.local>
2022-03-31 10:37:25 -0700

    init front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
+import Header from './components/Header.js'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './components/Footer.js'
+
+import {Container} from 'react-bootstrap'
+import './bootstrap.min.css'
+import HomeScreen from "./screens/HomeScreen"
+
+import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <BrowserRouter>
+            <Header/>
+            <main className={'py-3'}>
+            <Routes>
+                <Route path="/" element={<HomeScreen />} />
+                <Route path="/product/:id" element={< ProductScreen/>}/>
+            </Routes>
+            </main>
+            <footer/>
+        </BrowserRouter>
   );
 }
 
